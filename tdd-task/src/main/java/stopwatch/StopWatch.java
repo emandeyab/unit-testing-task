145dcaca548dad9aeb8efc8eb1db5b18ac63642c
emandeyab <emandeyab2003@gmail.com>
2025-08-13 13:38:40 +0300

    tdd-task

---
+++ tdd-task/src/main/java/stopwatch/StopWatch.java
@@ -0,0 +1,23 @@
+package stopwatch;
+
+public class StopWatch {
+    int minutes;
+    int hours;
+    int seconds;
+
+    public void record(int seconds) {
+
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+}
---
+++ tdd-task/src/test/java/stopwatch/StopWatchTest.java
@@ -0,0 +1,9 @@
+package stopwatch;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StopWatchTest {
+
+
+}
