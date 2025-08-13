8a940316154547755f2696773edcce6198bd4675
emandeyab <emandeyab2003@gmail.com>
2025-08-13 14:09:39 +0300

    RED: Add test for recording less than 60 seconds

--- tdd-task/src/test/java/stopwatch/StopWatchTest.java
+++ tdd-task/src/test/java/stopwatch/StopWatchTest.java
@@ -5,5 +5,18 @@ import org.junit.jupiter.api.Test;
 
 public class StopWatchTest {
 
+    @Test
+    void givenLessThan60Seconds_whenRecord_thenStoredAsSecondsOnly() {
+        // Arrange
+        StopWatch stopWatch = new StopWatch();
 
-}
+        //Act
+        stopWatch.record(35);
+
+        //Assert
+        Assertions.assertEquals(35, stopWatch.getSeconds());
+        Assertions.assertEquals(0, stopWatch.getMinutes());
+        Assertions.assertEquals(0, stopWatch.getHours());
+    }
+
+    }
