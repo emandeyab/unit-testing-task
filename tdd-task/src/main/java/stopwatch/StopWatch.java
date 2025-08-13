d067b5766638bb8911f8067dcd6e3d83b1cc6220
emandeyab <emandeyab2003@gmail.com>
2025-08-13 14:41:51 +0300

    Refactor: Clean up code

--- tdd-task/src/main/java/stopwatch/StopWatch.java
+++ tdd-task/src/main/java/stopwatch/StopWatch.java
@@ -9,23 +9,10 @@ public class StopWatch {
         if (seconds < 0) {
             throw new IllegalArgumentException("Time cannot be negative");
         }
-        if(seconds < 60){
-            this.seconds = seconds;
-            this.minutes = 0;
-            this.hours = 0;
-        }
-        if(seconds >= 60 && seconds < 3600){
-            this.seconds = seconds % 60;
-            this.minutes = seconds / 60;
-            this.hours = 0;
-        }
-        if(seconds >= 3600){
             this.hours = seconds / 3600;
             int remaining = seconds % 3600;
             this.seconds = remaining % 60;
             this.minutes = remaining / 60;
-
-        }
     }
 
     public int getHours() {
